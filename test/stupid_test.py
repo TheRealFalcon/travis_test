a13e352014ca46f0a66e7a398c10a92f650bab2b
James Falcon <therealfalcon@gmail.com>
2020-10-30 18:21:04 -0500

    .

---
+++ test/stupid_test.py
@@ -0,0 +1,27 @@
+import time
+
+def test1():
+    print("start test1")
+    time.sleep(60)
+    print("stop test1")
+
+def test2():
+    print("start test2")
+    time.sleep(60)
+    print("stop test2")
+
+def test3():
+    print("start test3")
+    time.sleep(60)
+    print("stop test3")
+
+def test4():
+    print("start test4")
+    time.sleep(60)
+    print("stop test4")
+
+def test5():
+    print("start test5")
+    time.sleep(60)
+    print("stop test5")
+
