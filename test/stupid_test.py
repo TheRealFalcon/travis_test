b7523cfaba3fab4e2cbc121b611ec35084a05ca9
James Falcon <therealfalcon@gmail.com>
2020-10-30 22:01:15 -0500

    .

--- test/stupid_test.py
+++ test/stupid_test.py
@@ -2,22 +2,22 @@ import time
 
 def test1():
     print("start test1")
-    time.sleep(60)
+    time.sleep(1)
     print("stop test1")
 
 def test2():
     print("start test2")
-    time.sleep(60)
+    time.sleep(900)
     print("stop test2")
 
 def test3():
     print("start test3")
-    time.sleep(60)
+    time.sleep(1)
     print("stop test3")
 
 def test4():
     print("start test4")
-    time.sleep(60)
+    time.sleep(1)
     print("stop test4")
 
 def test5():
